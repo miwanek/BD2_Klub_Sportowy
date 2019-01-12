f9e47e25a80b73b737d39dc1307101a2173bc954
Michal <m.iwanek@stud.elka.pw.edu.pl>
2019-01-12 01:48:12 +0100

    Initial SpringBoot save

---
+++ Aplikacja/app/src/main/java/bd2/app/AppApplication.java
@@ -0,0 +1,49 @@
+package bd2.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static javafx.application.Application.launch;
+
+@SpringBootApplication
+public class AppApplication extends Application {
+
+	private ConfigurableApplicationContext springContext;
+	private Parent rootNode;
+	private FXMLLoader fxmlLoader;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void init() throws Exception {
+		springContext = SpringApplication.run(AppApplication.class);
+		fxmlLoader = new FXMLLoader();
+		fxmlLoader.setControllerFactory(springContext::getBean);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		fxmlLoader.setLocation(getClass().getResource("/fxml/sample.fxml"));
+		rootNode = fxmlLoader.load();
+
+		primaryStage.setTitle("Hello World");
+		Scene scene = new Scene(rootNode, 800, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	@Override
+	public void stop() {
+		springContext.stop();
+	}
+
+}
+
