43279ce40d65656f282432e83741017f58e6a9bf
PatrycjaKarbownik <patrycja@karbownik.net>
2019-01-20 21:35:34 +0100

    few more inserts

---
+++ sql/insert_hall.sql
@@ -0,0 +1,11 @@
+INSERT INTO `hall` (`type`, `active`, `max_players`, `max_fans`, `rental_cost`, `facility_id`)
+	VALUES
+	('skocznia mala 1', 'T', NULL, 100000, 100000, 30),
+	('skocznia duza', 'T', NULL, 200000, 250000, 30),
+	('skocznia mala 2', 'T', NULL, 50000, 90000, 30),
+	('skocznia mala 1', 'T', NULL, 10000, 100000, 32),
+	('skocznia mala 2', 'T', NULL, 100000, 120000, 32),
+	('skocznia mala 3', 'T', NULL, 300000, 500000, 32),
+	('skocznia duza 1', 'T', NULL, 250000, 400000, 33),
+	('skocznia duza 2', 'T', NULL, 100000, 220000, 33),
+	('skocznia mala', 'T', NULL, 100000, 150000, 33);
---
+++ sql/insert_trainer.sql
@@ -0,0 +1,11 @@
+INSERT INTO `trainer` (`trainer_id`, `name`, `surname`, `powers`)
+	VALUES
+	(14, 'Gruszka', 'Zbigniew', NULL),
+	(15, 'Shulz', 'Oskar', NULL),
+	(16, 'Antonowicz', 'Dawid', NULL),
+	(17, 'Królik', 'Patryk', NULL),
+	(18, 'Zapomniały', 'Andrzej', NULL),
+	(19, 'Draska', 'Henryk', NULL),
+	(20, 'Opania', 'Tomasz', NULL),
+	(21, 'Onichowski', 'Edward', NULL),
+	(22, 'Lewak', 'Joachim', NULL);
