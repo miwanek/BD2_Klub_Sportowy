5c21394af1dbceca03eb0b955a1f6837fde2271d
PatrycjaKarbownik <patrycja@karbownik.net>
2019-01-22 01:42:12 +0100

    indexes created

--- sql/queries_done.sql
+++ sql/queries_done.sql
@@ -1,9 +1,20 @@
---średni wiek playerów w klubie
+-- lista zawodników drużyny tenisa chłopców z poziomu początkującego
+SELECT p.surname, p.name, p.birth_date
+FROM player p
+	JOIN player_team pt ON (p.representation_id = pt.player_representation_id)
+	JOIN team ON (team.representation_id = pt.team_representation_id)
+	JOIN tier ON (team.tier_id = tier.tier_id)
+	JOIN section s ON (team.section_id = s.section_id)
+	JOIN discipline d ON (s.discipline_id = d.discipline_id)
+WHERE p.sex = 'M' AND tier.name = 'początkujący' AND d.name = 'Tenis ziemny';
+
+
+-- średni wiek zawodników w klubie
 SELECT AVG(TIMESTAMPDIFF(YEAR, birth_date, CURDATE()))
 FROM player;
 
 
---ilość rozegranych rozgrywek w poszczególnych obiektach sportowych (może utrzymywanie którejś jest nieopłacalne)
+-- ilość rozegranych rozgrywek w poszczególnych obiektach sportowych (może utrzymywanie którejś jest nieopłacalne)
 SELECT sf.name, COUNT(g.game_id)
 FROM sport_facility sf
 	JOIN hall h ON (h.facility_id = sf.facility_id)
@@ -11,19 +22,7 @@ FROM sport_facility sf
 GROUP BY sf.name;
 
 
---lista trainerów z wypisywaniem zdobytych punktów na tournamentach wybranej dyscypliny(szukamy najlepszego trainera)
-SELECT d.name AS discipline, t.trainer_id, t.surname, t.name, SUM(tp.score) AS score
-FROM discipline d
-	JOIN trainer_discipline td ON (d.discipline_id = td.discipline_id)
-	JOIN trainer t ON (td.trainer_id = t.trainer_id)
-	JOIN representation_trainer rt ON (t.trainer_id = rt.trainer_id)
-	JOIN representation r ON (rt.representation_id = r.representation_id)
-	JOIN tournament_participation tp ON (r.representation_id = tp.representation_id)
-GROUP BY d.name, t.trainer_id
-ORDER BY d.name, score DESC;
-
-
---ilość playerów w wybranej dyscyplinie za określony rok (chcemy zobaczyć jakie dyscypliny są bardziej popularne)
+-- ilość zawodników w wybranej dyscyplinie za określony rok (chcemy zobaczyć jakie dyscypliny są bardziej popularne)
 -- 1 wersja
 SELECT YEAR(t.start_date) year, d.name, COUNT(DISTINCT p.representation_id) number_of_players
 FROM tournament t
@@ -47,28 +46,7 @@ FROM
 GROUP BY year, name;
 
 
---lista rozegranych rozgrywek wraz z wynikami w ramach Mistrzostw Świata w Piłkę Nożną w 2005 roku
-SELECT g.game_id, g.start_date, g.end_date, g.referee, r.representation_id, team.name, gp.place, gp.score
-FROM tournament t
-	JOIN game g ON (t.tournament_id = g.tournament_id)
-	JOIN game_participation gp ON (gp.game_id = g.game_id)
-	JOIN representation r ON (r.representation_id = gp.representation_id)
-	JOIN team ON (team.representation_id = r.representation_id)
-WHERE t.name = 'Mistrzostwa Świata Piłkę Nożną' AND YEAR(t.start_date) = 2005;
-
-
--- lista playerów drużyny tenisa chłopców z poziomu początkującego
-SELECT p.surname, p.name, p.birth_date
-FROM player p
-	JOIN player_team pt ON (p.representation_id = pt.player_representation_id)
-	JOIN team ON (team.representation_id = pt.team_representation_id)
-	JOIN tier ON (team.tier_id = tier.tier_id)
-	JOIN section s ON (team.section_id = s.section_id)
-	JOIN discipline d ON (s.discipline_id = d.discipline_id)
-WHERE p.sex = 'M' AND tier.name = 'początkujący' AND d.name = 'Tenis ziemny';
-
-
---średni wynik dziewczynek z poziomu zaawansowanego w podnoszeniu ciężarów w roku 2017
+-- średni wynik dziewczynek z poziomu zaawansowanego w podnoszeniu ciężarów w roku 2017
 SELECT AVG(gp.score)
 FROM discipline d
        JOIN player_discipline pd ON (d.discipline_id = pd.discipline_id)
@@ -78,4 +56,26 @@ FROM discipline d
        JOIN representation r ON (r.representation_id = p.representation_id)
        JOIN game_participation gp ON (gp.representation_id = r.representation_id)
        JOIN game g ON (g.game_id = gp.game_id)
-WHERE pg.sex = 'K' AND tier.name = 'zaawansowany' AND d.name = 'Podnoszenie ciężarów'
+WHERE pg.sex = 'K' AND tier.name = 'zaawansowany' AND d.name = 'Podnoszenie ciężarów' AND YEAR(g.start_date) = 2017
+
+
+-- lista trenerów z wypisywaniem zdobytych punktów na turniejach wybranej dyscypliny (szukamy najlepszego trenera)
+SELECT d.name AS discipline, t.trainer_id, t.surname, t.name, SUM(tp.score) AS score
+FROM discipline d
+	JOIN trainer_discipline td ON (d.discipline_id = td.discipline_id)
+	JOIN trainer t ON (td.trainer_id = t.trainer_id)
+	JOIN representation_trainer rt ON (t.trainer_id = rt.trainer_id)
+	JOIN representation r ON (rt.representation_id = r.representation_id)
+	JOIN tournament_participation tp ON (r.representation_id = tp.representation_id)
+GROUP BY d.name, t.trainer_id
+ORDER BY d.name, score DESC;
+
+
+-- lista rozegranych rozgrywek wraz z wynikami w ramach Mistrzostw Świata w Piłkę Nożną w 2005 roku
+SELECT g.game_id, g.start_date, g.end_date, g.referee, r.representation_id, team.name, gp.place, gp.score
+FROM tournament t
+	JOIN game g ON (t.tournament_id = g.tournament_id)
+	JOIN game_participation gp ON (gp.game_id = g.game_id)
+	JOIN representation r ON (r.representation_id = gp.representation_id)
+	JOIN team ON (team.representation_id = r.representation_id)
+WHERE t.name = 'Mistrzostwa Świata Piłkę Nożną' AND YEAR(t.start_date) = 2005;
